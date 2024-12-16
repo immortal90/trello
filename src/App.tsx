666035521f9bb47245f25cdb61b1f23512372ffe
Ihor Kopach <immortal90@meta.ua>
2024-12-16 21:58:49 +0200

    add second level

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,6 @@
-import { lazy, Suspense } from "react";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-} from "react-router-dom";
-import {ToastContainer} from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-
-const Board = lazy(() => import("./pages/Board/Board.tsx"));
-const Home = lazy(() => import("./pages/Home/Home.tsx"));
-const YouReady = lazy(() => import("./pages/Home/YouReady.tsx"));
 
 export default function App() {
     return (
-        <Router>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path="/" element={<YouReady />} />
-                    <Route path="/board" element={<Home />} />
-                    <Route path="/board/:board_id" element={<Board />} />
-                    <Route path="*" element={<div>Page Not Found</div>} />
-                </Routes>
-            </Suspense>
-            <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
-        </Router>
+        <>hello</>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter basename="/trello/">
-            hello
+            <App />
         </BrowserRouter>
     </StrictMode>,
 );
